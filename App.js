f5e434f67cc0cdb499281c2abfde756390c15088
Michael Morahan <morahan.mike@gmail.com>
2020-04-23 05:32:51 -0600

    adjust styling

--- App.js
+++ App.js
@@ -5,33 +5,35 @@ import styles from './styles/styles.js'
 function App(){
   const [outputText, setOutputText] = useState('Ready to get started!?')
   // console.log()
+
+  const logText = (input) => {
+    console.log(input)
+  }
     return (
-      <View style={styles.container}>
-        <View style={{padding: 25}}>
-          <Text style={styles.Text}>Logo</Text>
-          <Image source={require('./assets/icon.png')} style={{height: 117, width: 117}}/>
-          <Text style={styles.Text}>Welcome</Text>
+      <View style={styles.Screen}>
+        <View style={styles.head}>
+          <Image source={require('./assets/icon.png')} style={styles.log, {height: 117, width: 117, alignSelf: "center"}}/>
+          <Text style={styles.Welcome}>Welcome</Text>
         </View>
 
-        <View>
+        <View style={styles.LogIn}>
           <Text style={styles.Text}>Sign Into Spotify</Text>
-          <TextInput placeholder={'Email'}/>
+          <TextInput 
+            placeholder='Email'
+            style={styles.Input}
+            onChangeText={logText}
+
+            
+          />
           <View>
-            <Text style={styles.Text}>Log into Spotify Here</Text>
+            <Text style={styles.Text}>Log into Spotify Here below</Text>
           </View>
           <Text style={styles.Text}>Oops, I dont have an accout, im a dinosaur.</Text>
         </View>
 
         <View>
-          <Text>{outputText}</Text>
-          <Button
-            backgroundColor="#fff"
-            color="#000"
-            title="Change Text"
-            onPress={() =>
-              setOutputText("Sign into Spotify Below")
-            }
-          />
+          <Text style={styles.Text}>{outputText}</Text>
+          <Button title="button title here?" style={styles.Btn} onPress={() => setOutputText("Sign into Spotify Below")}/>
 
         </View>
       </View>
--- styles/styles.js
+++ styles/styles.js
@@ -1,20 +1,54 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  Screen: {
     flex: 1,
     backgroundColor: '#44ace8',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: "5%"
   },
-//   btn1: {
-//     background: '#smokewhite',
-//     color: '#ffff',
-//   },
-  Text: {
-    color: "#ffff",
-    textAlign: 'center',
 
+  Head: {
+    //: "35%",
+  },
+
+  Logo: {
+    height: 117, width: 117, alignSelf: "center", paddingTop: 100
+  },
+
+  Welcome: {
+    color: "#ffff",
+    fontSize: 38,
+    textAlign: 'center',
+    textShadowColor: "#000"
+  },
+
+  LogIn: {
+    backgroundColor: 'whitesmoke',
+    // alignItems: 'bottom',
+    justifyContent: 'center',
+    borderRadius: 17,
+    height: '25%',
+  },
+
+  Btn: {
+    shadowColor: 'whitesmoke',
+  },
+
+  Text: {
+    color: "#000",
+    textAlign: 'center',
+  },
+
+  Input: {
+    alignItems: 'center',
+    width: "80%",
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+    padding: 10,
+    padding: '10%',
+    alignSelf: 'center',
   }
 });
 
