3ac18cf32b420116294007f88c65af8697b032a7
Michael Morahan <morahan.mike@gmail.com>
2020-04-23 06:53:07 -0600

    created add song component

---
+++ components/addSong.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Text, View, TextInput, ScrollView, Picker, Slider, Switch, TouchableOpacity, Button, Image, SafeAreaView } from 'react-native';
+import styles from '../styles/styles.js';
+
+
+function addSong() {
+    const [searchedSong, setSearchedSong] = useState('');
+    const [songList, setSongList] = useState([]);
+
+    const songSearchHandler = (searchedSong) => {
+        console.log(searchedSong);
+    }
+    return (
+        <View style={styles.Search}>
+            <Text style={styles.Text}>Search Spotify for Track</Text>
+            <TextInput
+                placeholder='track name'
+                style={styles.Input}
+                onChangeText={searchedSong}
+            />
+            <Button title="search" style={styles.Btn} />
+        </View>
+    );
+}
+
+export default addSong;
